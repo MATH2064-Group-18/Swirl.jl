5ac018f58e5e4cd35d65ea874a8e251165b73c27
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-18 16:10:54 +0800

    Add Jacobi solve method

--- src/PressureSolve.jl
+++ src/PressureSolve.jl
@@ -1,3 +1,28 @@
 module PressureSolve
 
+function jacobi!(f, f_old, g, collision, dx, maxIterations)
+    @assert ndims(f) == ndims(f_old) == ndims(g) == ndims(collision) == length(dx)
+    
+    dxn2 = @. (one(dx) / dx)^2
+    c0 = 0.5 / sum(dxn2)
+    c = c0 * dxn2
+
+    d = ndims(f)
+    n = length(f)
+
+    for iter = 1:maxIterations
+        copy!(f_old, f)
+
+        for i = 1:n
+            A = 0
+            for (j, stride) in enumerate(strides(f))
+                a1 = collision > 0 ? f_old[i-stride] : f_old[i]
+                a2 = collision > 0 ? f_old[i+stride] : f_old[i]
+                A += c[j] * (a1 + a2)
+            end
+            f[i] = A - c0 * g[i]
+        end
+    end
+end
+
 end # module
