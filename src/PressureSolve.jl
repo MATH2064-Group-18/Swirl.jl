5d8afa8d8d4df89bc45983cb5a9599eb70dbc95f
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-19 20:39:26 +0800

    Fix collision check indexing

--- src/PressureSolve.jl
+++ src/PressureSolve.jl
@@ -11,12 +11,11 @@ function jacobi!(f, f_old, g, collision, dx, maxIterations)
 
     for iter = 1:maxIterations
         copy!(f_old, f)
-
         for i = 1:n
             A = 0
             for (j, stride) in enumerate(strides(f))
-                a1 = collision > 0 ? f_old[i-stride] : f_old[i]
-                a2 = collision > 0 ? f_old[i+stride] : f_old[i]
+                a1 = collision[i-stride] > 0 ? f_old[i-stride] : f_old[i]
+                a2 = collision[i+stride] > 0 ? f_old[i+stride] : f_old[i]
                 A += c[j] * (a1 + a2)
             end
             f[i] = A - c0 * g[i]
