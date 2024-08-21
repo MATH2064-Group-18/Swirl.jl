2d6ac6448c89aae03c9237be4d53ad6477a0216c
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-21 10:40:35 +0800

    Add FluidSolve module

---
+++ src/FluidSolve.jl
@@ -0,0 +1,18 @@
+module FluidSolve
+
+include("Fluids.jl")
+include("PressureSolve.jl")
+include("Advection.jl")
+include("Projection.jl")
+
+
+function timestepUpdate!(fluid::Fluid, dt)
+    vel_old = similar(fluid.vel)
+    copy!(vel_old, fluid.vel)
+    Advection.advectVector!(fluid.vel, vel_old, fluid.collision, fluid.dx, dt)
+    projectNonDivergent!(fluid)
+    Advection.advectScalar!(fluid.density, fluid.vel, fluid.collision, fluid.dx, dt)
+end
+
+
+end # module
