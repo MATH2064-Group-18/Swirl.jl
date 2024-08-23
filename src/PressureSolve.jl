411664c7bd98b5017748a4d0d87ea995fcae4f2b
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-23 17:51:00 +0800

    Add Gauss-Seidel solve method

--- src/PressureSolve.jl
+++ src/PressureSolve.jl
@@ -25,4 +25,28 @@ function jacobi!(f, f_old, g, collision, dx, maxIterations)
     end
 end
 
+function gaussSeidel!(f, g, collision, dx, maxIterations)
+    @assert ndims(f) == ndims(g) == ndims(collision) == length(dx)
+    
+    dxn2 = @. (one(dx) / dx)^2
+    c0 = 0.5 / sum(dxn2)
+    c = c0 * dxn2
+
+    n = length(f)
+
+    for iter = 1:maxIterations
+        for i = 1:n
+            if collision[i] > 0
+                A = 0
+                for (j, strid) in enumerate(strides(f))
+                    a1 = collision[i-strid] > 0 ? f[i-strid] : f[i]
+                    a2 = collision[i+strid] > 0 ? f[i+strid] : f[i]
+                    A += c[j] * (a1 + a2)
+                end
+                f[i] = A - c0 * g[i]
+            end
+        end
+    end
+end
+
 end # module
