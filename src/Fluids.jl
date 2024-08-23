744d1c3c5538e926a73546da95f97f865265e71c
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-23 18:00:45 +0800

    Add type parameters to Fluid struct
    Parametric types now used for Fluid struct. Struct members now
    have a concrete type, and performance has significantly improved.

--- src/Fluids.jl
+++ src/Fluids.jl
@@ -1,10 +1,10 @@
 
 #lonely struct
 
-mutable struct Fluid
-    dx
-    vel
-    collision
-    p
-    density
+mutable struct Fluid{T<:AbstractFloat, N, NN}
+    dx::Vector{T}
+    vel::Array{T, NN}
+    collision::Array{T, N}
+    p::Array{T, N}
+    density::Array{T, N}
 end
