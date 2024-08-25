be2249dd5fedc4f401f495851247ae954c0ff73a
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-25 14:56:20 +0800

    Add conjugate gradient solve method

--- src/PressureSolve.jl
+++ src/PressureSolve.jl
@@ -1,5 +1,7 @@
 module PressureSolve
 
+using LinearAlgebra
+
 function jacobi!(f, f_old, g, collision, dx, maxIterations)
     @assert ndims(f) == ndims(f_old) == ndims(g) == ndims(collision) == length(dx)
     
@@ -46,6 +48,61 @@ function gaussSeidel!(f, g, collision, dx, maxIterations)
                 f[i] = A - c0 * g[i]
             end
         end
+    end
+end
+
+function conjugateGradient!(f, g, collision, dx, maxIterations)
+    dxn2 = @. (one(dx) / dx)^2
+
+    n = length(f)
+    
+    v = similar(f)
+    p = similar(f)
+    r = zeros(eltype(f), size(f))
+
+
+    for i in eachindex(f)
+        if collision[i] > 0
+            r[i] = g[i]
+            for (j, s) in enumerate(strides(f))
+                a1 = collision[i-s] > 0 ? f[i-s] : f[i]
+                a2 = collision[i+s] > 0 ? f[i+s] : f[i]
+                r[i] -= (a1 + a2 - 2 * f[i]) * dxn2[j]
+            end
+        end
+    end
+
+    copy!(p, r)
+
+    res_sum = sum(abs2, r)
+    iter = 0
+    while iter < maxIterations
+        for i in eachindex(collision)
+            v[i] = 0
+            if collision[i] > 0
+                for (j, s) in enumerate(strides(f))
+                    a1 = collision[i-s] > 0 ? p[i-s] : p[i]
+                    a2 = collision[i+s] > 0 ? p[i+s] : p[i]
+                    v[i] += (a1 + a2 - 2 * p[i]) * dxn2[j]
+                end
+            end
+        end
+        
+        α = dot(r, r) / dot(p, v)
+        
+        f += α * p
+        
+        
+        r -= α * v
+        
+        res_sum_old = res_sum
+        res_sum = sum(abs2, r)
+        
+        β = res_sum / res_sum_old
+
+        p = r + β * p
+        
+        iter += 1
     end
 end
 
